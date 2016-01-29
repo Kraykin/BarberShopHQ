309cf2946418edff812fb3bc49c7ca8b9d5077d5
Kraykin <kraykin@gmail.com>
2016-01-29 15:28:37 +0300

    Add visit.erb

--- app.rb
+++ app.rb
@@ -12,7 +12,26 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@client_name = params[:client_name]
+	@client_phone = params[:client_phone]
+	@client_date = params[:client_date]
+	@barber = params[:barber]
+	@color = params[:color]
+
+
+
+	erb "Спасибо! #{@client_name.capitalize}, мы будем ждать вас #{@client_date}"
 end
