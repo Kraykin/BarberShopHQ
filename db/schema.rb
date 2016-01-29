8a241a618979d8f92e7c354a1ef5c945c9a052ce
Kraykin <kraykin@gmail.com>
2016-01-29 19:27:31 +0300

    Add Contacts migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129083738) do
+ActiveRecord::Schema.define(version: 20160129154207) do
 
   create_table "barbers", force: :cascade do |t|
     t.text     "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20160129083738) do
     t.text     "datestamp"
     t.text     "barber"
     t.text     "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text     "email"
+    t.text     "contact"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
